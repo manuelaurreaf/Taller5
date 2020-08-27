ca638810451cb7ce06d0ab6257632365612d2ac2
manuelaurreaf <53128590+manuelaurreaf@users.noreply.github.com>
2020-08-27 15:47:42 -0500

    Add files via upload

---
+++ ControladorPersonaje.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorPersonaje : MonoBehaviour
+{
+    public string descripcion = string.Empty ;
+    public string nombre = string.Empty ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ EstudiarParcial.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EstudiarParcial : MonoBehaviour
+{
+    public List<ControladorPersonaje> listaPersonaje;
+    public GameObject personajeActual;
+    private int indexList;
+
+    public Text descripcionPersonaje;
+    public InputField nombrePersonaje;
+
+    public Button nombreMio;
+    public Text cambioNombre;
+
+    private Animator animatorActual;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            LastPersonaje();
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            NextPersonaje();
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            animatorActual.SetBool("CambioAnim", true);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            animatorActual.SetBool("CambioAnim", false);
+        }
+    }
+
+    public void NextPersonaje()
+    {
+        indexList++;
+
+        if (personajeActual != null) Destroy(personajeActual);
+
+        indexList = indexList % listaPersonaje.Count;
+        personajeActual = Instantiate(listaPersonaje[indexList].gameObject);
+        
+
+        descripcionPersonaje.text = listaPersonaje[indexList].descripcion;
+        nombrePersonaje.text = listaPersonaje[indexList].descripcion;
+
+        animatorActual = personajeActual.GetComponent<Animator>();
+    }
+    public void LastPersonaje()
+    {
+        indexList++;
+
+        if (personajeActual != null) Destroy(personajeActual);
+
+        indexList = indexList % listaPersonaje.Count;
+        personajeActual = Instantiate(listaPersonaje[indexList].gameObject);
+
+        descripcionPersonaje.text = listaPersonaje[indexList].descripcion;
+        nombrePersonaje.text = listaPersonaje[indexList].descripcion;
+
+        animatorActual = personajeActual.GetComponent<Animator>();
+    }
+    public void CambiarNombre()
+    {
+        cambioNombre.text = "Bienvenido " + nombrePersonaje.text;
+    }
+}
