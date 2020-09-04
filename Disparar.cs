0706d65848f1f6e4bb3d20994691a1e456634f04
manuelaurreaf <53128590+manuelaurreaf@users.noreply.github.com>
2020-09-03 19:47:57 -0500

    Add files via upload

---
+++ Disparar.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disparar : MonoBehaviour
+{
+    public GameObject bola;
+    public float fuerzay = 0;
+    public float fuerzaz = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+            /* if (Input.GetKey(KeyCode.Space))
+             {
+                 bola.transform.position += new Vector3(0, 50, 100);
+                 Debug.Log("  w w w  ");
+             }*/
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+            GameObject bolanueva;
+           
+            bolanueva = Instantiate(bola, transform.position, Quaternion.identity);
+            bolanueva.GetComponent<Rigidbody>().AddForce(new Vector3(0, fuerzay, fuerzaz));
+        }
+            
+    }
+
+}
---
+++ NuevaParticula.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NuevaParticula : MonoBehaviour
+{
+    public ParticleSystem particleSystem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider collider)
+    {
+        particleSystem.Play(true);
+        Debug.Log(message: "Entrando al trigger");
+    }
+}
