61bb9a865d9434ba637548df39a705ba4f276fea
manuelaurreaf <53128590+manuelaurreaf@users.noreply.github.com>
2020-09-17 19:34:21 -0500

    Add files via upload

---
+++ Controlcambiocamara.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controlcambiocamara : MonoBehaviour
+{
+    public SceneController controladorescena; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        controladorescena.AsignarCamara(1, 10);
+    }
+}
---
+++ SceneController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class SceneController : MonoBehaviour
+{
+    public CinemachineVirtualCamera camara1;
+    public CinemachineVirtualCamera camara2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    { // revisando que con botones me cambie la camara de una a otra
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            AsignarCamara(10, 1);
+        }
+        else if (Input.GetKeyDown(KeyCode.X))
+        {
+            AsignarCamara(1, 10);
+        }
+    }
+    public void AsignarCamara (int prioridad1, int prioridad2)
+    {
+        camara1.Priority = prioridad1;
+        camara2.Priority =prioridad2;
+    }
+}
