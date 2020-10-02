45895e095884a9e5b3452e43b69a350051207950
manuelaurreaf <53128590+manuelaurreaf@users.noreply.github.com>
2020-10-01 19:45:49 -0500

    Add files via upload

---
+++ Ejemplo.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejemplo : MonoBehaviour
+{
+    private Coroutine recordatorioCoroutine;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+            EjemploCiclo();
+        else if (Input.GetKeyDown(KeyCode.D))
+            recordatorioCoroutine = StartCoroutine(Recordatorio());
+        else if (Input.GetKeyDown(KeyCode.W))
+            //StopAllCoroutines();
+            StopCoroutine(recordatorioCoroutine);
+        else if (Input.GetKeyDown(KeyCode.T))
+            StartCoroutine(RecordatorioRecurrente());
+
+    }
+
+    public void EjemploCiclo()
+    {
+        for (int i = 0; i < 100000; i++)
+        {
+            Debug.Log("prueba");
+        }
+    }
+
+
+    IEnumerator Contador()
+    {
+        for (int i = 0; i < 100000; i++)
+        {
+            yield return null;
+            Debug.Log("prueba1");
+        }
+    }
+
+    IEnumerator Recordatorio()
+    {
+        Debug.Log("Agregando Recordatorio");
+        yield return new WaitForSeconds(5f);
+        Debug.Log("Hola! no olvides eso tan importante que tienes que hacer");
+
+    }
+
+    IEnumerator RecordatorioRecurrente()
+    {
+        while (true)
+        {
+            Debug.Log("Agregando Recordatorio recurrente");
+            yield return new WaitForSeconds(6f);
+            Debug.Log("Hola! no te voy dejar olvidar esa cosa");
+
+            //yield return new WaitForSeconds(1f);
+        }
+
+    }
+
+
+}
---
+++ Ejemplo2.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ejemplo2 : MonoBehaviour
+{
+    public Text TextoUsado;
+
+    public string textoString;
+    public string textoActual;
+    public float segundos = 1f;
+
+    public List<string> textoCharla;
+    public int posicionCharlaActual = 0;
+
+    private bool estoyEscribiendo = false;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.A)) //sale el texto de una 
+            TextoUsado.text = textoString;
+        else if (Input.GetKeyUp(KeyCode.S))
+            StartCoroutine(CargarTexto(textoString));
+        else if (Input.GetKeyUp(KeyCode.D))
+            StartCoroutine(Conversacion());
+        else if (Input.GetKeyUp(KeyCode.F))
+            StartCoroutine(Conversacion2());
+        else if (Input.GetKeyUp(KeyCode.Space))
+            StartCoroutine(Conversacion3());
+
+    }
+
+    private IEnumerator CargarTexto(string textoDeseado)
+    {
+        textoActual = string.Empty;
+        for (int i = 0; i < textoDeseado.Length; i++)
+        {
+            //textoActual = textoActual + textoDeseado[i];
+            textoActual += textoDeseado[i];
+
+            TextoUsado.text = textoActual;
+            yield return new WaitForSeconds(segundos);
+        }
+
+    }
+
+    private IEnumerator Conversacion() //se borra el texto anterior
+    {
+        //foreach (string charla in textoCharla)
+        for (int e = 0; e < textoCharla.Count; e++)
+        {
+            string textoDeseado = textoCharla[e];
+            textoActual = string.Empty;
+            for (int i = 0; i < textoDeseado.Length; i++)
+            {
+                //textoActual = textoActual + textoDeseado[i];
+                textoActual += textoDeseado[i];
+
+                TextoUsado.text = textoActual;
+                yield return new WaitForSeconds(segundos);
+            }
+
+            yield return new WaitForSeconds(1f);
+
+        }
+
+    }
+
+    private IEnumerator Conversacion2()// sale el texto con enter entre dialogo
+    {
+        textoActual = string.Empty;
+        //foreach (string charla in textoCharla)
+        for (int e = 0; e < textoCharla.Count; e++)
+        {
+            string textoDeseado = textoCharla[e];
+            for (int i = 0; i < textoDeseado.Length; i++)
+            {
+                //textoActual = textoActual + textoDeseado[i];
+                textoActual += textoDeseado[i];
+
+                TextoUsado.text = textoActual;
+                yield return new WaitForSeconds(segundos);
+            }
+
+            yield return new WaitForSeconds(1f);
+            textoActual += "\n";
+        }
+
+    }
+
+    private IEnumerator Conversacion3() //sale el texto con enter y con tener que undir botones para siguiente
+    {
+        if (!estoyEscribiendo)
+        {
+            estoyEscribiendo = true;
+            string textoDeseado = textoCharla[posicionCharlaActual];
+            for (int i = 0; i < textoDeseado.Length; i++)
+            {
+                //textoActual = textoActual + textoDeseado[i];
+                textoActual += textoDeseado[i];
+
+                TextoUsado.text = textoActual;
+                yield return new WaitForSeconds(segundos);
+            }
+
+            posicionCharlaActual++;
+            if (posicionCharlaActual == textoCharla.Count)
+                posicionCharlaActual = 0;
+
+            textoActual += "\n";
+            estoyEscribiendo = false;
+        }
+
+    }
+}
