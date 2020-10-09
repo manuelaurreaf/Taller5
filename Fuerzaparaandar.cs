0a4b5eb51e659190cb234249dc28e4d9e73fa9e7
manuelaurreaf <53128590+manuelaurreaf@users.noreply.github.com>
2020-10-08 19:43:39 -0500

    Add files via upload

---
+++ Fuerzaparaandar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fuerzaparaandar : MonoBehaviour
+{
+    // public int z = 0;
+   // public GameObject personaje;
+    public static int movespeed = 1;
+    public Vector3 userDirection = Vector3.right;
+    
+    public void Update()
+    {
+        transform.Translate(userDirection * movespeed * Time.deltaTime);
+    }
+    public void caminemija()
+    {
+        
+    }
+   public void OnTriggerStay(Collider other)
+    {
+        // gameObject.SetActive(false);
+        transform.eulerAngles = new Vector3(0, -90, 0);
+    }
+
+}
